00ff018e5ffec9a970cf18ee6546126ec7234ad4
alextotalk <alexoo@inbox.ru>
2022-04-29 18:05:05 +0300

    1HW

---
+++ src/p2-homeworks/h1/HW1.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import m from "./Message.module.css"
+import Message from "./Message";
+
+const messageData = {
+    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
+    name: 'Some Name',
+    message: 'some text',
+    time: '22:00',
+}
+
+function HW1() {
+    return (
+        <div className={m.hw}>
+            <hr/>
+            homeworks 1
+
+            should work (должно работать)
+
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
+
+            <hr/>
+            {/*для личного творчества, могу проверить*/}
+            {/*<AlternativeMessage/>*/}
+            <hr/>
+        </div>
+    )
+}
+
+export default HW1
---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import m from './Message.module.css'
+
+function Message(props: any) {
+    return (
+        <div className={m.message}>
+            <div className={m.item}>
+                <div className={m.ava}>
+                    <img
+                        src={props.avatar}
+                        alt=""/>
+                </div>
+                <div className={m.blockMessageWrapper}>
+                    <div className={m.tail}></div>
+                    <div className={m.blockMessage}>
+                        <div className={m.blockMessageItem}>
+                            <div className={m.blockMessageName}>{props.name}</div>
+                            <div className={m.blockMessageText}>{props.message}</div>
+                        </div>
+                        <div className={m.blockMessageTime}>{props.time}</div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Message
