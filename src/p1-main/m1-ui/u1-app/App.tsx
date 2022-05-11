d896a206bd8c6fb7cdf3a93e0465c2d8a7b04fc2
alextotalk <alexoo@inbox.ru>
2022-05-11 12:12:36 +0300

    HW-3 is done.

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import s from './App.module.css'
-import HW2 from "../../../p2-homeworks/h2/HW2";
+import HW3 from "../../../p2-homeworks/h3/HW3";
 
 function App() {
     return (
         <div className={s.App}>
             <div>react homeworks:</div>
             {/*<HW1/>*/}
-            <HW2/>
-            {/*<HW3/>*/}
+            {/*<HW2/>*/}
+            <HW3/>
             {/*<HW4/>*/}
             {/*<HW5/>*/}
 
--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,26 +1,28 @@
-import React from 'react'
+import React, {ChangeEvent} from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
+    name: string // need to fix any
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void // need to fix any
     addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    error: string // need to fix any
+    totalUsers: number // need to fix any
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+    let inputClass = error === '' ? s.good : s.error // need to fix with (?:)
 
     return (
         <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
+            <input value={name} onChange={setNameCallback}
+                   className={`${inputClass} ${s.onFocus}`}/>
             <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
+            <button onClick={addUser} className={inputClass}>add</button>
+            <pre> {totalUsers} - Count user names !</pre>
+
         </div>
     )
 }
